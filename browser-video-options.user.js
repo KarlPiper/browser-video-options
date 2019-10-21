4e2205a3c896cf8b9f0fe3d7d91d0097b92f3f48
KarlPiper <9043540+KarlPiper@users.noreply.github.com>
2019-10-21 09:38:15 -0400

    add keyby and screenfull

--- browser-video-options.user.js
+++ browser-video-options.user.js
@@ -8,7 +8,7 @@
 // @grant          none
 // @require        https://code.jquery.com/jquery-3.4.0.min.js
 // @icon           https://i.imgur.com/CxT2VtA.png
-// @version        3.0
+// @version        4.0
 // ==/UserScript==
 
 /*═════════════════╦═══════════════════════════════════╦════════════╗
@@ -60,17 +60,132 @@ var showSettings = true;
 var customCss = ``;
 /*═════════════════════════════════════════════════════════════════*/
 
+/*!
+ * Keyby v0.0.1
+ * Copyright (c) 2016 Petros G. Sideris
+ * Licensed under the MIT license
+ */
+(function(root, factory) {
+	var define;
+	if (typeof define === 'function' && define.amd)	define([], factory);
+	else if (typeof exports === 'object') module.exports = factory();
+	else root.keyby = factory();
+} (this, function() {
+	var self = this;
+	this.map = {
+		8: 'backspace',
+		9: 'tab',
+		13: 'enter',
+		16: 'shift',
+		17: 'ctrl',
+		18: 'alt',
+		20: 'capslock',
+		27: 'esc',
+		32: 'space',
+		33: 'pageup',
+		34: 'pagedown',
+		35: 'end',
+		36: 'home',
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down',
+		45: 'ins',
+		46: 'del',
+
+		48: '0',
+		49: '1',
+		50: '2',
+		51: '3',
+		52: '4',
+		53: '5',
+		54: '6',
+		55: '7',
+		56: '8',
+		57: '9',
+
+		65: 'a',
+		66: 'b',
+		67: 'c',
+		68: 'd',
+		69: 'e',
+		70: 'f',
+		71: 'g',
+		72: 'h',
+		73: 'i',
+		74: 'j',
+		75: 'k',
+		76: 'l',
+		77: 'm',
+		78: 'n',
+		79: 'o',
+		80: 'p',
+		81: 'q',
+		82: 'r',
+		83: 's',
+		84: 't',
+		85: 'u',
+		86: 'v',
+		87: 'w',
+		88: 'x',
+		89: 'y',
+		90: 'z',
+
+		106: '*',
+		107: '+',
+		109: '-',
+		110: '.',
+		111 : '/',
+		186: ';',
+		187: '=',
+		188: ',',
+		189: '-',
+		190: '.',
+		191: '/',
+		192: '`',
+		219: '[',
+		220: '\\',
+		221: ']',
+		222: '\''
+	};
+	var keyby = {version: '0.0.1'};
+	var keycall = {};
+	keyby.on = function(key, callback){
+		key += '';
+		key = key.toLowerCase();
+		keycall[key] = callback;
+		return this;
+	};
+	window.addEventListener('keyup', function(e){
+		e.preventDefault();
+		for(var key in keycall)
+			if(key == self.map[e.which])
+				keycall[key]()
+	});
+	return keyby;
+}));
+/*!
+ * screenfull
+ * v5.0.0 - 2019-09-09
+ * (c) Sindre Sorhus; MIT License
+ */
+!function(){"use strict";var u="undefined"!=typeof window&&void 0!==window.document?window.document:{},e="undefined"!=typeof module&&module.exports,t=function(){for(var e,n=[["requestFullscreen","exitFullscreen","fullscreenElement","fullscreenEnabled","fullscreenchange","fullscreenerror"],["webkitRequestFullscreen","webkitExitFullscreen","webkitFullscreenElement","webkitFullscreenEnabled","webkitfullscreenchange","webkitfullscreenerror"],["webkitRequestFullScreen","webkitCancelFullScreen","webkitCurrentFullScreenElement","webkitCancelFullScreen","webkitfullscreenchange","webkitfullscreenerror"],["mozRequestFullScreen","mozCancelFullScreen","mozFullScreenElement","mozFullScreenEnabled","mozfullscreenchange","mozfullscreenerror"],["msRequestFullscreen","msExitFullscreen","msFullscreenElement","msFullscreenEnabled","MSFullscreenChange","MSFullscreenError"]],l=0,r=n.length,t={};l<r;l++)if((e=n[l])&&e[1]in u){for(l=0;l<e.length;l++)t[n[0][l]]=e[l];return t}return!1}(),r={change:t.fullscreenchange,error:t.fullscreenerror},n={request:function(r){return new Promise(function(e,n){var l=function(){this.off("change",l),e()}.bind(this);this.on("change",l),r=r||u.documentElement,Promise.resolve(r[t.requestFullscreen]()).catch(n)}.bind(this))},exit:function(){return new Promise(function(e,n){if(this.isFullscreen){var l=function(){this.off("change",l),e()}.bind(this);this.on("change",l),Promise.resolve(u[t.exitFullscreen]()).catch(n)}else e()}.bind(this))},toggle:function(e){return this.isFullscreen?this.exit():this.request(e)},onchange:function(e){this.on("change",e)},onerror:function(e){this.on("error",e)},on:function(e,n){var l=r[e];l&&u.addEventListener(l,n,!1)},off:function(e,n){var l=r[e];l&&u.removeEventListener(l,n,!1)},raw:t};t?(Object.defineProperties(n,{isFullscreen:{get:function(){return Boolean(u[t.fullscreenElement])}},element:{enumerable:!0,get:function(){return u[t.fullscreenElement]}},isEnabled:{enumerable:!0,get:function(){return Boolean(u[t.fullscreenEnabled])}}}),e?module.exports=n:window.screenfull=n):e?module.exports={isEnabled:!1}:window.screenfull={isEnabled:!1}}();
+
 $(function(){
+	//global vars
+	var vid = $('video');
+	var src = vid.find('source');
+
 	//detect MIME type
-	if ($('source').attr('type').split('/')[0] === 'video') {
+	if (src.attr('type').split('/')[0] === 'video') {
 		videoFile();
-	} else if ($('source').attr('type').split('/')[0] === 'audio') {
+	} else if (src.attr('type').split('/')[0] === 'audio') {
 		audioFile();
 	}
 
 	//start with blank video element
 	function replaceVideo() {
-		$('video').replaceWith(function () {
+		vid.replaceWith(function () {
 			return $('<video>').append($(this).contents());
 		});
 	}
@@ -108,6 +223,31 @@ $(function(){
 		if (muted) $('video')[0].muted = true;
 		$('video').attr({disablepictureinpicture, controls, controlslist, autoplay, loop, width, height, poster}).prop('volume', volume);
 	}
+
+	function changeVol(n) {
+		var currentVol = $('video').prop('volume') * 100;
+		var changeVol = ((currentVol+n)/100).toFixed(2);
+		if (changeVol > 1 || changeVol < 0) {
+			return false;
+		} else {
+			$('video').prop('volume', changeVol);
+		}
+	}
+
+	//custom keybindings
+	keyby.on('down', function(){
+		changeVol(-10);
+	}).on('up', function () {
+		changeVol(10);
+	}).on('m', function() {
+		if ($('video')[0].muted === true) {
+			$('video')[0].muted = false;
+		} else {
+			$('video')[0].muted = true;
+		}
+	}).on('f', function() {
+		screenfull.toggle($('video')[0]);
+	});
 
 	//show debug info
 	if (showSettings) console.log('╔═══════════════════════════════════╗\n║ Browser Video Options: Debug Info ║\n╚═══════════════════════════════════╝\n║ loop'+' = '+loop+'\n║ autoplay'+' = '+autoplay+'\n║ muted'+' = '+muted+'\n║ controls'+' = '+controls+'\n║ noPip'+' = '+noPip+'\n║ noDownload'+' = '+noDownload+'\n║ noRemotePlayback'+' = '+noRemotePlayback+'\n║ noFullscreen'+' = '+noFullscreen+'\n║ width'+' = '+width+'\n║ height'+' = '+height+'\n║ poster'+' = '+poster+'\n║ volume'+' = '+volume+'\n║ audioLoop'+' = '+audioLoop+'\n║ audioAutoplay'+' = '+audioAutoplay+'\n║ audioMuted'+' = '+audioMuted+'\n║ audioControls'+' = '+audioControls+'\n║ audioVolume'+' = '+audioVolume+'\n║ showSettings'+' = '+showSettings+'\n║ customCss'+' = '+customCss)
